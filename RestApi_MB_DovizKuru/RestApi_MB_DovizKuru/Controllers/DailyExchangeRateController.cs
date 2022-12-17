ca9458ffdc353222053bb434ed8e5678370a8395
KandemirOkan <110767934+KandemirOkan@users.noreply.github.com>
2022-12-17 15:06:59 +0300

    Rest Api - CB Current Rates

---
+++ RestApi_MB_DovizKuru/RestApi_MB_DovizKuru/Controllers/DailyExchangeRateController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+namespace RestApi_MB_DovizKuru.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DailyExchangeRateController : ControllerBase
+    {
+        [HttpPost]
+        public ResponseData Run(RequestData request)
+        {
+            ResponseData result = new ResponseData();
+            try
+            {
+                string tcmblink = string.Format("https://www.tcmb.gov.tr/kurlar/{0}.xml", (request.IsToday) ? "today" : string.Format("{2}{1}/{0}{1}{2}", request.Day.ToString().PadLeft(2, '0'), request.Month.ToString().PadLeft(2, '0'), request.Year.ToString()));
+                result.Data = new List<ResponseDataDovizKuru>();
+
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(tcmblink);
+                if (doc.SelectNodes("Tarih_Date").Count<1)
+                {
+                    result.Error = "Currency information not found.";
+                    return result;
+                }
+                foreach (XmlNode node in doc.SelectNodes("Tarih_Date")[0].ChildNodes)
+                {
+                    ResponseDataDovizKuru kur = new ResponseDataDovizKuru();
+
+                    kur.Code = node.Attributes["Kod"].Value;
+                    kur.Name = node["Isim"].InnerText;
+                    kur.Unit = int.Parse(node["Unit"].InnerText);
+                    kur.BuyingRate = Convert.ToDecimal("0" + node["ForexBuying"].InnerText.Replace(".", ","));
+                    kur.SellingRate = Convert.ToDecimal("0" + node["ForexSelling"].InnerText.Replace(".", ","));
+                    kur.EffectiveBuyingRate = Convert.ToDecimal("0" + node["BanknoteBuying"].InnerText.Replace(".", ","));
+                    kur.EffectiveSellingRate = Convert.ToDecimal("0" + node["BanknoteSelling"].InnerText.Replace(".", ","));
+                    result.Data.Add(kur);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Error = ex.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ RestApi_MB_DovizKuru/RestApi_MB_DovizKuru/Models.cs
@@ -0,0 +1,29 @@
+﻿namespace RestApi_MB_DovizKuru
+{
+    public class RequestData
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public bool IsToday { get; set; }
+
+    }
+    public class ResponseDataDovizKuru
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Unit { get; set; }
+        public decimal BuyingRate { get; set; }
+        public decimal SellingRate { get; set; }
+        public decimal EffectiveBuyingRate { get; set; }
+        public decimal EffectiveSellingRate { get; set; }
+
+    }
+    public class ResponseData
+    {
+        public List<ResponseDataDovizKuru> Data { get; set; }
+
+        public string Error { get; set; }
+
+    }
+}
